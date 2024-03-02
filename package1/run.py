5bb1ae738ca7d8f73a9942f1cf75fab12b3ea1bb
vvvv1111vvvv <105331246+vvvv1111vvvv@users.noreply.github.com>
2024-03-03 06:54:56 +0900

    feat: add pratice

--- package1/run.py
+++ package1/run.py
@@ -1,6 +1,8 @@
 #for development test
-#from app import app
 
 
-#if __name__ == '__main__':
+# from app import app
+
+
+# if __name__ == '__main__':
 #    app.run(port=5001, host ='0.0.0.0', debug=True)
